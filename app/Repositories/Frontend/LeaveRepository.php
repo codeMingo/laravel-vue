13ab1d4ed9a28be91c9a480b3fb42b59a9b6e59c
linlianmin <linlianmin@eoffice.com>
2017-11-23 17:46:54 +0800

    留言模块修改

--- app/Repositories/Frontend/LeaveRepository.php
+++ app/Repositories/Frontend/LeaveRepository.php
@@ -2,40 +2,37 @@
 namespace App\Repositories\Frontend;
 
 use App\Models\Leave;
-use App\Repositories\Frontend\DictRepository;
-use Illuminate\Support\Facades\Auth;
 
 class LeaveRepository extends BaseRepository
 {
-    public $table_name = 'articles';
 
     /**
-     * 文章列表
+     * 留言列表
      * @param  Array $input [search_form]
      * @return Array
      */
     public function lists($input)
     {
-        $resultData['lists'] = $this->getLeaveLists();
-        return [
-            'status'  => Parent::SUCCESS_STATUS,
-            'data'    => $resultData,
-            'message' => '数据获取成功',
-        ];
+        $result['lists'] = $this->getLeaveLists($input['search']);
+        return $this->responseResult(true, $result);
     }
 
-    public function getLeaveLists()
+    /**
+     * 获取列表
+     * @return Object
+     */
+    public function getLeaveLists($search)
     {
-        $audit_pass_value = DictRepository::getInstance()->getValueByCodeAndTextEn('audit', 'pass');
-        $leave_lists      = Leave::where('is_audit', $audit_pass_value)->where('status', 1)->where('parent_id', 0)->with('user')->paginate(10);
-        if ($leave_lists->isEmpty()) {
-            return $leave_lists;
-        }
-        $leave_ids = [];
-        foreach ($leave_lists as $index => $leave) {
-            $leave_ids[] = $leave->id;
+        $dicts = $this->getRedisDictLists(['audit' => ['pass']]);
+        $result = Leave::where('is_audit', $dicts['audit']['pass'])->where('status', 1)->where('parent_id', 0)->with('user')->paginate();
+        if ($result->isEmpty()) {
+            return [];
         }
 
+        $leave_ids = [];
+        foreach ($result as $index => $leave) {
+            $leave_ids[] = $leave->id;
+        }
         // 找出所有的回复
         if (!empty($leave_ids)) {
             $response_lists = Leave::whereIn('parent_id', $leave_ids)->with('user')->where('status', 1)->get();
@@ -44,77 +41,53 @@ class LeaveRepository extends BaseRepository
                 foreach ($response_lists as $index => $response) {
                     $response_temp[$response->parent_id][] = $response;
                 }
-
-                foreach ($leave_lists as $index => $leave) {
-                    $leave_lists[$index]['response'] = isset($response_temp[$leave->id]) ? $response_temp[$leave->id] : [];
+                foreach ($result as $index => $leave) {
+                    $result[$index]['response'] = isset($response_temp[$leave->id]) ? $response_temp[$leave->id] : [];
                 }
             }
         }
-        return $leave_lists;
+        return $result;
     }
 
+    /**
+     * 留言
+     * @param  Array $input [leave_id, content] 留言数据
+     * @return Array
+     */
     public function publish($input)
     {
-        $leave_id = isset($input['leave_id']) ? intval($input['leave_id']) : '';
+        $leave_id = isset($input['leave_id']) ? intval($input['leave_id']) : 0;
         $content  = isset($input['content']) ? strval($input['content']) : '';
         if (!$content) {
-            return [
-                'status'  => Parent::ERROR_STATUS,
-                'data'    => [],
-                'message' => '操作失败，内容不得为空',
-            ];
+            return $this->responseResult(false, [], '留言失败，参数错误，请刷新后重试');
         }
-        $leave_audit = DictRepository::getInstance()->getValueByCodeAndTextEn('system', 'leave_audit');
 
-        $dict_key_value_lists = DictRepository::getInstance()->getKeyValueByCode('audit');
+        $dicts = $this->getRedisDictLists(['system' => ['leave_audit'], 'audit' => ['loading', 'pass']]);
         // 表示回复
         if ($leave_id) {
-            $leave_list = Leave::where('id', $leave_id)->where('status', 1)->where('is_audit', $dict_key_value_lists['pass'])->first();
-            if (empty($leave_list)) {
-                return [
-                    'status'  => Parent::ERROR_STATUS,
-                    'data'    => [],
-                    'message' => '回复失败，回复的留言为空',
-                ];
+            $list = Leave::where('id', $leave_id)->where('status', 1)->where('is_audit', $dicts['audit']['pass'])->first();
+            if (empty($list)) {
+                return $this->responseResult(false, [], '留言失败，参数错误，请刷新后重试');
             }
         }
-        $user_id      = Auth::guard('web')->id();
-        $createResult = Leave::create([
-            'user_id'    => $user_id,
-            'parent_id'  => $leave_id ? $leave_id : 0,
+        $result['list'] = Leave::create([
+            'user_id'    => $this->getUserId(),
+            'parent_id'  => $leave_id,
             'content'    => $content,
-            'is_audit'   => $leave_audit ? $dict_key_value_lists['loading'] : $dict_key_value_lists['pass'],
+            'is_audit'   => $dicts['system']['leave_audit'] ? $dicts['audit']['loading'] : $dicts['audit']['pass'],
             'ip_address' => getClientIp(),
-            'status'     => 1,
         ]);
 
         // 记录操作日志
         Parent::saveOperateRecord([
             'action' => 'Leave/publish',
             'params' => $input,
-            'text'   => $createResult ? ($leave_id ? '回复成功' : '留言成功') : ($leave_id ? '回复失败，未知错误' : '留言失败，未知错误'),
-            'status' => !!$createResult,
+            'text'   => $leave_id ? '回复成功' : '留言成功',
         ]);
 
-        if (!$createResult) {
-            return [
-                'status'  => Parent::ERROR_STATUS,
-                'data'    => [],
-                'message' => $leave_id ? '回复失败，未知错误' : '留言失败，未知错误',
-            ];
-        }
-        // 留言成功
-        if (!$leave_audit) {
-            $publish_leave_list                = Leave::where('id', $createResult->id)->with('user')->first();
-            $publish_leave_list->response      = [];
-            $publish_leave_list->show_response = true;
-        }
-        return [
-            'status'  => Parent::SUCCESS_STATUS,
-            'data'    => [
-                'list' => $leave_audit ? [] : $publish_leave_list,
-            ],
-            'message' => $leave_id ? '回复成功' : '留言成功',
-        ];
+        $result['list']['response']      = [];
+        $result['list']['show_response'] = true;
+        $result['list']['user']          = DB::table('users')->where('id', $this->getUserId())->first();
+        return $this->responseResult(true, $result, $leave_id ? '回复成功' : '留言成功');
     }
 }
